42921914f81485c6186c89e4e32f635803786492
cris2d <cristofferlian2@gmail.com>
2026-06-14 12:37:01 -0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var config = {
         "Entre todas elas, o lugar especial que você tem no meu coração.",
         "seu jeito faz com que tudo seja um pouco mais bonito.",
         "Você se tornou uma parte muito importante da minha vida,",
-        "alguém que admiro, valorizo e amo cada dia mais."
+        "alguém que admiro, valorizo e amo cada dia mais.",
         "Eu te amo. ❤️",
     ],
     // Formato: "YYYY-MM-DDTHH:MM:SS"
