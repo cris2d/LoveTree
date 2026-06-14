d0e037f26d1f55ff2d58b5150387b75f1e98ee26
cris2d <cristofferlian2@gmail.com>
2026-06-14 10:48:48 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
-// Configuração personalizada
 var config = {
-    // As frases abaixo não precisam ser exatamente 7, 
-    // você pode colocar 4, 2 ou quantas quiser
     lines: [
         "Meu amor,",
         "As flores florescem e o tempo passa, mas algumas coisas permanecem.",
@@ -10,15 +7,11 @@ var config = {
         "e seu jeito faz com que tudo seja um pouco mais bonito.",
         "Você se tornou uma parte muito importante da minha vida,",
         "alguém que admiro, valorizo e amo cada dia mais.",
-        "seria a felicidade de ter você ao meu lado.",
+        "Seria a felicidade de ter você ao meu lado.",
         "Eu te amo. ❤️",
     ],
-    // Data em que se apaixonaram - IMPORTANTE: não altere o formato!
-    // Formato: Ano-Mês-DiaThora:minuto:segundo
-    //      Avisos importantes:
-    //          1. Horas, minutos e segundos devem ter sempre dois dígitos  ex: 00:00:00
-    //          2. O "T" não pode ser removido
+    // Formato: "YYYY-MM-DDTHH:MM:SS"
     date: "2025-12-10T18:52:00",
-    // Nome dos dois
-    names: ["Vitória e Cristoffer"]
+    // IMPORTANTE: names deve ser um array com dois nomes separados
+    names: ["Vitória", "Cristoffer"]
 }
